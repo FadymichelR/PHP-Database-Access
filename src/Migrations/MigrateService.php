9ee1fe3f8ff8ec0c343e93ad4ebd908e19058630
Fady Michel <fadymichel.r@gmail.com>
2019-05-04 00:14:07 +0200

    improvement migration

--- src/Migrations/MigrateService.php
+++ src/Migrations/MigrateService.php
@@ -3,11 +3,12 @@
 
 namespace Fad\Migrations;
 
-
+/**
+ * Class MigrateService
+ * @package Fad\Migrations
+ */
 class MigrateService
 {
-
-
     /**
      * @var \PDO
      */
@@ -19,32 +20,82 @@ class MigrateService
 
     /**
      * MigrateService constructor.
-     * @param \PDO $pdo
      * @param array $params
      */
-    public function __construct(\PDO $pdo, array $params = [])
+    public function __construct(array $params = [])
     {
-        $this->pdo = $pdo;
+        $this->pdo = $params['connection'];
         $defaultParams = [
             'table_name' => 'migration_versions',
-            'column_name' => 'version',
         ];
         $this->params = $defaultParams + $params;
     }
 
+    /**
+     * @return void
+     */
+    /**
+     * @return void
+     */
+    public function generateMigration(): void
+    {
+        $file = date('YmdHis') . '.sql';
+        file_put_contents($this->params['migrations_directory'] . DIRECTORY_SEPARATOR . $file, '');
+        echo 'Migration ' . $file . ' generate' . PHP_EOL;
+    }
+
+    /**
+     * @param string|null $version
+     */
+    public function migrate(): void
+    {
+        $this->createVersion();
+
+        $stmt = $this->pdo->prepare('SELECT version FROM ' . $this->params['table_name']);
+        $stmt->execute();
+        $versions = $stmt->fetchAll(\PDO::FETCH_COLUMN);
+
+        foreach ($this->getMigrations() as $version => $migration) {
+
+            if (in_array($version, $versions)) {
+                continue;
+            }
+            echo 'Migrating ' . $version . PHP_EOL;
+            try {
+                $this->pdo->query(file_get_contents($migration));
+                $status = $this->pdo->prepare('INSERT INTO ' . $this->params['table_name'] . ' (`version`) VALUES (:version)')
+                    ->execute(['version' => $version]);
+            } catch (\Exception $e) {
+                var_dump($e->getMessage());
+            }
+
+        }
+
+    }
 
     /**
      * @return void
      */
-    public function generateMigration() :void
+    public function createVersion(): void
     {
-        $migration = file_get_contents(__DIR__.'migration.txt');
-        $version = 'Version'.date('YmdHis');
-        $migration = str_replace(
-            ['%migrations_namespace%', '%version%'],
-            [$this->params['migrations_namespace'], $version],
-            $migration
-        );
-        file_put_contents($this->params['migrations_directory'].DIRECTORY_SEPARATOR.$version.'.php', $migration);
+        $this->pdo->query('CREATE TABLE IF NOT EXISTS ' . $this->params['table_name'] . ' (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, version varchar(255) NOT NULL)');
+    }
+
+    /**
+     * @param string $folder
+     * @return array
+     */
+    private function getMigrations(): array
+    {
+        $migrations = [];
+        foreach (new \DirectoryIterator($this->params['migrations_directory']) as $file) {
+            if ($file->getExtension() !== 'sql') {
+                continue;
+            }
+            $version = pathinfo($file->getBasename(), PATHINFO_FILENAME);
+            $migrations[$version] = $file->getPathname();
+        }
+        ksort($migrations);
+        return $migrations;
     }
 }
